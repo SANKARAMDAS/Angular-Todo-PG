1488584aca524103934cbf43b3081695b1c030a3
SANKARAM <das95sankaram@gmail.com>
2020-11-15 16:29:09 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-list';
+  title = 'Todo-List';
+  todos =[
+    {label:'Bring Water', done:false, priority: 3},
+    {label:'Turn on the Light', done:true, priority: 5},
+    {label:'Say Hi', done:true, priority: 2},
+    {label:'Go to Bed', done:false, priority: 1},
+];
+
+addTodo(newTodoLabel){
+  var newTodo = {
+    label: newTodoLabel,
+    priority: 1,
+    done: false
+  };
+  this.todos.push(newTodo);
+}
+
+deleteTodo(todo){
+  this.todos= this.todos.filter(t => t.label !==todo.label);
+}
 }
